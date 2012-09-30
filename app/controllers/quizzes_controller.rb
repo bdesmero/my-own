3b47823015f46c1d73f4453706ab61ddc84aa0f1
biaesmero <besmero@clinic-it.com>
2012-09-30 12:32:55 -0400

    Checking of quiz

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -2,6 +2,21 @@ class QuizzesController < ApplicationController
   
   def new
     @word = Word.random
+    @quiz = Word.new
+    raise params.inspect
+  end
+
+
+  def create
+    #raise (Word.check_quiz(params)).inspect
+    @word = Word.random
+    @quiz = Word.new
+    
+    if Word.check_quiz(params) == 1
+      redirect_to root_path
+    else
+      redirect_to new_quiz_path, :note => 'Wrong answer'
+    end
   end
 
 
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,3 +1,14 @@
 class Word < ActiveRecord::Base
   attr_accessible :example_translation, :example, :term, :translation
+  
+  
+  def self.check_quiz params
+    word = self.find params[:word_id]
+    if params[:translation] == word.translation
+      return 1
+    else
+      return 0
+    end
+  end
+  
 end
