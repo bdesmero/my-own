674ee4f6aafb48bf727db948a22a03ccf0ee7c4b
Bia Esmero <besmero@clinic-it.com>
2013-02-24 14:30:00 +0800

    Show correct answer on Quizzes

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -2,11 +2,26 @@
 // All this logic will automatically be available in application.js.
 
 $(document).ready(function(){
-
+  
   $('#example-link').on('click', function(e){
     var display = $('#quiz-example').css('display');
     if(display == 'none'){ $('#quiz-example').slideDown(); }
     else{ $('#quiz-example').slideUp(); }
+  });
+
+  
+  $('.submit').on('click', function(e){
+    var answer = $('.quiz-answer').val(),
+      correctAnswer = $('#translation').val();
+      
+    if (answer == correctAnswer){
+      $('.quiz-answer').attr('id', 'inputWarning');
+    }else {
+      if ($('.correct-answer').css('display') == 'none'){  
+        e.preventDefault();
+        $('.correct-answer').slideDown();  
+      }
+    }
   });
 
 });
